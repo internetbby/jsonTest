b9bf64d8a89d4e2c48f9b065710d67c7eb26d654
Linnea <linnea.alse@elev.ga.ntig.se>
2021-10-06 14:39:57 +0200

    initial commit

---
+++ json-test/Fighter.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace json_test
+{
+    public class Fighter
+    {
+        public string Name {get; set;}
+        
+        public string WeaponUsed{get; set;}
+
+        public int Health{get; set;}
+
+    }
+}
---
+++ json-test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Text.Json; 
+
+namespace json_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
