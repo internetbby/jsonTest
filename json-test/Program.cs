67b773a2999181811d7df3f9516b09e259230173
Linnea <linnea.alse@elev.ga.ntig.se>
2021-10-06 14:47:36 +0200

    added player writing

--- json-test/Program.cs
+++ json-test/Program.cs
@@ -9,6 +9,35 @@ namespace json_test
         static void Main(string[] args)
         {
 
+            Fighter f1 = new Fighter();
+
+            Console.WriteLine("Choose name");
+
+           f1.Name =  Console.ReadLine();
+
+           Console.WriteLine("Write what weapon they use");
+
+           f1.WeaponUsed = Console.ReadLine();
+           
+           System.Console.WriteLine("What's their hp?");
+
+           string healthChosen = Console.ReadLine();
+
+           bool tryParse = int.TryParse(healthChosen, out int healthInt);
+
+           while(tryParse != true){
+               System.Console.WriteLine("not a number. Write again");
+
+               healthChosen = Console.ReadLine();
+               tryParse = int.TryParse(healthChosen, out  healthInt);
+           }
+            
+
+            f1.Health = healthInt; 
+
+            string jsonFile = JsonSerializer.Serialize<Fighter>(f1);
+
+            
 
         }
     }
